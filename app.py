e11615d620fac4c0e2f2bfcec9da22f471426e87
vishnupriya-shisode <140819184+vishnupriya-shisode@users.noreply.github.com>
2025-05-04 11:15:41 +0100

    removed the while loop

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
-import time
-
-
 def say_hello():
-    print("Hello from DevSecOPs!!!")
-
+    print("Hello from DevSecOps!!!")
 
 if __name__ == "__main__":
     say_hello()
-    while True:
-        time.sleep(1000)
